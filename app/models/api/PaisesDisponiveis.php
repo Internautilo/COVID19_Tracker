1ef9c787556162d19eee77f5ba07a821aab578d2
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 15:35:03 -0300

    feat: created Paises Disponiveis API class

---
+++ app/models/api/PaisesDisponiveis.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Api;
+use App\Models\Api\Interfaces\ApiInterface;
+
+class PaisesDisponiveis implements ApiInterface
+{
+    public function getResponse(string|null $param): array
+    {
+        try {
+            $response = file_get_contents("https://dev.kidopilabs.com.br/exercicio/covid.php?listar_paises=1");
+            $response = json_decode($response);
+
+            foreach ($response as $pais) {
+                $paises[] = $pais;
+            };
+
+            return $paises;
+        } catch (\Throwable $th) {
+            throw new \Exception($th->getMessage());
+        }
+    }
+}
