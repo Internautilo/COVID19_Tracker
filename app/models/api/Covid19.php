8a31471a9eabb79acb63300dcab56658e7c29cc3
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 10:59:04 -0300

    refactor: getResponse() method now treats its param for better usability

--- app/models/api/Covid19.php
+++ app/models/api/Covid19.php
@@ -5,10 +5,11 @@ use App\Models\Api\Interfaces\ApiInterface;
 
 class Covid19 implements ApiInterface
 {
-    public function getResponse(string|null $param): array
+    public function getResponse(string|null $pais): array
     {
         try {
-            $response = file_get_contents('https://dev.kidopilabs.com.br/exercicio/covid.php?pais=Brazil');
+            $pais = ucwords(strtolower($pais));
+            $response = file_get_contents("https://dev.kidopilabs.com.br/exercicio/covid.php?pais={$pais}");
             $response = json_decode($response);
 
             foreach ($response as $estado) {
