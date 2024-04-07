a36df53e4e730aebcbb9b33f008371c806cbe25d
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 18:40:34 -0300

    feat: added loading background to all pages

--- shared/navbar.php
+++ shared/navbar.php
@@ -39,3 +39,76 @@
     </div>
   </div>
 </nav>
+
+<style>
+  .loading-img {
+    width: 200px;
+    height: 200px;
+    object-fit: contain;
+    margin-bottom: 5px;
+  }
+
+  .pre-loader {
+    position: fixed;
+    z-index: 3000;
+    /** make sure this is the highest value compared to all other divs **/
+    top: 0;
+    left: 0;
+    background: #000937;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+  }
+
+  .pre-loader.hidden {
+    animation: fadeOut 0.8s ease-in-out forwards;
+
+  }
+
+  @keyframes fadeOut {
+    100% {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+
+  .custom-loader {
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    background:
+      radial-gradient(farthest-side, #0172cf 94%, #0000) top/8px 8px no-repeat,
+      conic-gradient(#0000 30%, #0172cf);
+    -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 8px), #000 0);
+    animation: s3 1s infinite linear;
+  }
+
+  @keyframes s3 {
+    100% {
+      transform: rotate(360deg)
+    }
+  }
+</style>
+
+
+
+<div class='pre-loader'>
+  <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" fill="currentColor" class="bi bi-capsule mb-5"
+    viewBox="0 0 16 16">
+    <path
+      d="M1.828 8.9 8.9 1.827a4 4 0 1 1 5.657 5.657l-7.07 7.071A4 4 0 1 1 1.827 8.9Zm9.128.771 2.893-2.893a3 3 0 1 0-4.243-4.242L6.713 5.429z" />
+  </svg>
+  <div class="custom-loader"></div>
+</div>
+
+
+<script type='text/javascript'>
+  window.addEventListener('load', function () {
+    setTimeout(function () {
+      document.querySelector('.pre-loader').className += ' hidden';
+    }, 1000)
+  });
+</script>
