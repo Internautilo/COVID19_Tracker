e31758f68f2049fed8385ea06aca074c84557df1
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 14:34:52 -0300

    feat: created template for the main view

--- index.php
+++ index.php
@@ -8,9 +8,62 @@
     <?php require_once __DIR__ . "/shared/navbar.php"; ?>
 </head>
 
-<body class="d-flex flex-column min-vh-100" data-bs-theme="dark">
+<body data-bs-theme="dark">
 
-    <hr class="hr invisible" style="height: 900px;">
+    <div class="container-fluid justify-content-center align-items-center">
+
+        <div class="row">
+            <div class="col-sm text-center mb-5">
+                <form action="" method="post">
+                    <div class="container text-center mt-5">
+                        <h3>Seleção de País</h3>
+                    </div>
+                    <div class="container">
+                        <select class="form-select mt-5 mb-3" aria-label="Seleção de País">
+                            <option selected>Selecionar pais</option>
+                            <option value="Argentina">Argentina</option>
+                            <option value="Brazil">Brasil</option>
+                            <option value="Canada">Canada</option>
+                        </select>
+                        <button type="submit" class="btn btn-primary">Selecionar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <?php if (isset($_POST['pais'])) { ?>
+            <div class="row">
+                <div class="col-sm mt-5">
+                    <div class="container">
+                        <div class="mt-5">
+                            <label for="confirmados" class="form-label">Casos Confirmados</label>
+                            <input type="text" class="form-control mb-3" id="confirmados" disabled readonly
+                                placeholder="Número de Mortes">
+                            <label for="mortes" class="form-label">Mortes</label>
+                            <input type="text" class="form-control" id="mortes" disabled readonly
+                                placeholder="Número de Mortes">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php } else { ?>
+            <div class="row">
+                <div class="col-sm mt-5">
+                    <div class="container">
+                        <div class="mt-5">
+                            <label for="confirmados" class="form-label">Casos Confirmados</label>
+                            <input type="text" class="form-control mb-3" id="confirmados" disabled readonly
+                                placeholder="Número de Mortes">
+                            <label for="mortes" class="form-label">Mortes</label>
+                            <input type="text" class="form-control" id="mortes" disabled readonly
+                                placeholder="Número de Mortes">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+
+    </div>
+
 
 </body>
 
