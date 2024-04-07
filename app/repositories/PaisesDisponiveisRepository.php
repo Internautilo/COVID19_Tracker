f27bd6bed071720cfcb14d88f058ab5f90a11a95
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 15:41:22 -0300

    feat: implemented Api Repository Interface into Paises Disponiveis Repository

--- app/repositories/PaisesDisponiveisRepository.php
+++ app/repositories/PaisesDisponiveisRepository.php
@@ -5,14 +5,20 @@ use App\Models\Api\PaisesDisponiveis;
 use App\Repositories\Interfaces\ApiRepositoryInterface;
 use Database\DbConnection;
 
-class PaisesDisponiveisRepository
+class PaisesDisponiveisRepository implements ApiRepositoryInterface
 {
     private PaisesDisponiveis $__model;
     private \PDO $__connection;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->__model = new PaisesDisponiveis;
         $this->__connection = DbConnection::getConnection();
         $this->__connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function getResponse(string|null $param): array
+    {
+        return $this->__model->getResponse();
     }
 }
