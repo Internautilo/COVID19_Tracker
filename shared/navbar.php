98fed19e0372d7d607556171e54ead1050557681
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 18:21:23 -0300

    refactor: nav buttons now redirect corretly

--- shared/navbar.php
+++ shared/navbar.php
@@ -19,7 +19,7 @@
     <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">COVID-19 Tracker
+          <a class="nav-link active" aria-current="page" href="/">COVID-19 Tracker
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-houses"
               viewBox="0 0 16 16">
               <path
@@ -28,7 +28,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Bonus <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+          <a class="nav-link" href="bonus.php">Bonus <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
               fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
               <path
                 d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
