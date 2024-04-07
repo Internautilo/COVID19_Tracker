deb96bad5b985c61f5ff330422a671317e90b66c
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 17:33:30 -0300

    feat: added country display after selecting

--- bonus.php
+++ bonus.php
@@ -26,7 +26,7 @@ $apiPaises = new PaisesDisponiveisController;
         <div class="container text-center mt-5">
             <h2>Selecione dois paises</h2>
         </div>
-        <form action="." method="post">
+        <form action="./bonus.php" method="post">
             <div class="row justify-content-center">
                 <div class="col-sm-2 text-center mb-5">
                     <div class="container">
@@ -47,7 +47,6 @@ $apiPaises = new PaisesDisponiveisController;
                         <select class="form-select mt-5 mb-3" id="pais2" name="pais2" aria-label="Seleção de País">
                             <option selected>Selecionar pais</option>
                             <?php
-                            $paises = $apiPaises->getResponse();
                             foreach ($paises as $pais) { ?>
                                 <option value="<?= $pais ?>">
                                     <?= $pais ?>
@@ -61,41 +60,44 @@ $apiPaises = new PaisesDisponiveisController;
                 </div>
             </div>
         </form>
+        <div class="row justify-content-around mt-5">
 
-        <?php if (isset($_POST['pais'])) { ?>
-            <div class="row justify-content-center">
-                <div class="col-sm-2 my-5 bg-success text-center rounded rounded-2">
-                    <h1>
-                        <?= $_POST['pais'] ?>
-                    </h1>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <?php
-                $response = $apiCovid->getResponse($_POST['pais']);
+            <?php if (isset($_POST['pais1']) && isset($_POST['pais2'])) {
+                $i = 0;
+                for ($i = 0; $i < 2; $i++) {
+                    $pais = $_POST["pais" . $i + 1];
 
-                foreach ($response as $estado) {
-                    ?>
-                    <div class="col-sm-2 mt-2 mb-3 mx-2 border border-2 rounded pb-4">
+                    $response = $apiCovid->getResponse($pais);
+
+                    $totalConfirmados = 0;
+                    $totalMortos = 0;
+                    foreach ($response as $estado) {
+                        $totalConfirmados += $estado->Confirmados;
+                        $totalMortos += $estado->Mortos;
+                    } ?>
+
+
+                    <div class="col-sm-5 mt-2 mb-3 mx-2 border border-2 rounded pb-4">
                         <div class="container">
                             <div class="mt-2">
-                                <h5 class="bold">
-                                    <?= $estado->ProvinciaEstado ?>
-                                </h5>
+                                <h2 class="bold">
+                                    <?= $estado->Pais ?>
+                                </h2>
                             </div>
                             <div class="mt-3">
                                 <label for="confirmados" class="form-label">Casos Confirmados</label>
                                 <input type="text" class="form-control mb-3" id="confirmados" disabled readonly
-                                    placeholder="Número de Mortes" value="<?= $estado->Confirmados ?>">
+                                    placeholder="Número de Mortes" value="<?= $totalConfirmados ?>">
                                 <label for="mortes" class="form-label">Mortes</label>
                                 <input type="text" class="form-control" id="mortes" disabled readonly
-                                    placeholder="Número de Mortes" value="<?= $estado->Mortos ?>">
+                                    placeholder="Número de Mortes" value="<?= $totalMortos ?>">
                             </div>
                         </div>
                     </div>
+
                 <?php } ?>
+                <hr class="invisible" style="height: 100px">
             </div>
-            <hr class="invisible" style="height: 100px">
 
         <?php } else { ?>
             <div class="row">
