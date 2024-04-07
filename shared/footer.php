97358bf735365823d19e1d66abd247ec36321173
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 13:41:05 -0300

    feat: changed footer

--- shared/footer.php
+++ shared/footer.php
@@ -1,7 +1,9 @@
-<footer class="footer mt-auto py-3 bg-body-tertiary fixed-bottom">
-    <div class="container">
+<footer class="footer mt-auto py-2 bg-body-tertiary fixed-bottom">
+    <div class="container text-center">
         <span class="text-body-secondary">Place sticky footer content here.</span>
     </div>
 </footer>
 
-<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
