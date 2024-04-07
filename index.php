441b95d643785f93752d98d3fa9f2fde0c444bb7
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 15:23:53 -0300

    feat: added condition to display covid api data

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+use App\Services\Covid19Service;
+
+require_once "./vendor/autoload.php";
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -14,14 +24,14 @@
 
         <div class="row">
             <div class="col-sm text-center mb-5">
-                <form action="" method="post">
+                <form action="." method="post">
                     <div class="container text-center mt-5">
                         <h3>Seleção de País</h3>
                     </div>
                     <div class="container">
-                        <select class="form-select mt-5 mb-3" aria-label="Seleção de País">
+                        <select class="form-select mt-5 mb-3" id="pais" name="pais" aria-label="Seleção de País">
                             <option selected>Selecionar pais</option>
-                            <option value="Argentina">Argentina</option>
+                            <option value="Australia">Australia</option>
                             <option value="Brazil">Brasil</option>
                             <option value="Canada">Canada</option>
                         </select>
@@ -30,21 +40,43 @@
                 </form>
             </div>
         </div>
+
         <?php if (isset($_POST['pais'])) { ?>
-            <div class="row">
-                <div class="col-sm mt-5">
-                    <div class="container">
-                        <div class="mt-5">
-                            <label for="confirmados" class="form-label">Casos Confirmados</label>
-                            <input type="text" class="form-control mb-3" id="confirmados" disabled readonly
-                                placeholder="Número de Mortes">
-                            <label for="mortes" class="form-label">Mortes</label>
-                            <input type="text" class="form-control" id="mortes" disabled readonly
-                                placeholder="Número de Mortes">
-                        </div>
-                    </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-2 my-5 bg-success text-center rounded rounded-2">
+                <h1>
+                    <?= $_POST['pais'] ?>
+                </h1>
                 </div>
             </div>
+            <div class="row justify-content-center">
+                <?php
+                $apiCovid = new Covid19Service;
+                $response = $apiCovid->getResponse($_POST['pais']);
+
+                foreach ($response as $estado) {
+                    ?>
+                    <div class="col-sm-2 mt-2 mb-3 mx-2 border border-2 rounded pb-4">
+                        <div class="container">
+                            <div class="mt-2">
+                                <h5 class="bold">
+                                    <?= $estado->ProvinciaEstado ?>
+                                </h5>
+                            </div>
+                            <div class="mt-3">
+                                <label for="confirmados" class="form-label">Casos Confirmados</label>
+                                <input type="text" class="form-control mb-3" id="confirmados" disabled readonly
+                                    placeholder="Número de Mortes" value="<?= $estado->Confirmados ?>">
+                                <label for="mortes" class="form-label">Mortes</label>
+                                <input type="text" class="form-control" id="mortes" disabled readonly
+                                    placeholder="Número de Mortes" value="<?= $estado->Mortos ?>">
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+            <hr class="invisible" style="height: 100px">
+
         <?php } else { ?>
             <div class="row">
                 <div class="col-sm mt-5">
