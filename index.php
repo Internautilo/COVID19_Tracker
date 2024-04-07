3f1cd1629ba2f8547fd7a532f3be19cc666525ee
Internautilo <yuribertulucci2004@gmail.com>
2024-04-06 21:15:00 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COVID-19 Tracker</title>
+    <?php require_once __DIR__ . "/shared/navbar.php"; ?>
+</head>
+
+<body class="d-flex flex-column min-vh-100" data-bs-theme="dark">
+
+    <hr class="hr invisible" style="height: 900px;">
+
+</body>
+
+<footer>
+    <?php require_once __DIR__ . '/shared/footer.php'; ?>
+</footer>
+
+</html>
---
+++ shared/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer mt-auto py-3 bg-body-tertiary fixed-bottom">
+    <div class="container">
+        <span class="text-body-secondary">Place sticky footer content here.</span>
+    </div>
+</footer>
+
+<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
