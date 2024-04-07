10e036edf131c2788bfe7be394b4f8a129310e29
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 10:48:13 -0300

    feat: created Covid19 class implementing the ApiInterface

---
+++ app/models/api/Covid19.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Api;
+
+use App\Api\Interfaces\ApiInterface;
+
+class Covid19 implements ApiInterface
+{
+    public function getResponse(string|null $param): array
+    {
+        try {
+            $response = file_get_contents('https://dev.kidopilabs.com.br/exercicio/covid.php?pais=Brazil');
+            $response = json_decode($response);
+
+            foreach ($response as $estado) {
+                $estados[$estado->ProvinciaEstado] = $estado;
+            };
+
+            return $estados;
+        } catch (\Throwable $th) {
+            throw new \Exception($th->getMessage());
+        }
+    }
+}
