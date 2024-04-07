3b66810d878ad80d027a6557054e8bb93d6ce699
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 11:51:17 -0300

    feat: added getResponse to Covid19Repository

--- app/repositories/Covid19Repository.php
+++ app/repositories/Covid19Repository.php
@@ -1,20 +1,41 @@
-<?php 
+<?php
 namespace App\Repositories;
 
 use App\Models\Api\Covid19;
 use App\Repositories\Interfaces\ApiRepositoryInterface;
 use Database\DbConnection;
 
-class Covid19Repository
+class Covid19Repository implements ApiRepositoryInterface
 {
     private Covid19 $__model;
     private \PDO $__connection;
 
-    public function __construct(Covid19 $covid19) {
-        $this->__model = $covid19;
+    public function __construct()
+    {
+        $this->__model = new Covid19;
         $this->__connection = DbConnection::getConnection();
         $this->__connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
 
-    
+    public function getResponse(string|null $pais): array
+    {
+        $response = $this->__model->getResponse($pais);
+
+        try {
+            date_default_timezone_set("America/Sao_Paulo");
+            $date = date('Y-m-d H:i:s');
+
+            $query = "INSERT INTO consultaAPI (pais, data) VALUES (:pais, :data)";
+
+            $stmt = $this->__connection->prepare($query);
+            $stmt->bindParam(':pais', $pais);
+            $stmt->bindParam(':data', $date);
+
+            $stmt->execute();
+
+            return $response;
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
