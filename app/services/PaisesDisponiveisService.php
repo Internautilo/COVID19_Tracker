b9a5f9700ac9c463d92d85894250f885d6300eb6
Internautilo <yuribertulucci2004@gmail.com>
2024-04-07 15:47:04 -0300

    feat: created Paises Disponiveis Service

---
+++ app/services/PaisesDisponiveisService.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\PaisesDisponiveisRepository;
+use App\Services\Interfaces\ApiServiceInterface;
+
+class PaisesDisponiveisService implements ApiServiceInterface
+{
+    private readonly PaisesDisponiveisRepository $__repository;
+
+    public function __construct()
+    {
+        $this->__repository = new PaisesDisponiveisRepository;
+    }
+
+    public function getResponse(string|null $param): array
+    {
+        return $this->__repository->getResponse();
+    }
+}
