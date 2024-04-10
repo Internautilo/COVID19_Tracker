ec4cb47ee312b7a90bebd4e993b6d52f97f9455e
Internautilo <yuribertulucci2004@gmail.com>
2024-04-10 15:35:17 -0300

    fix: added create and use to .sql file

--- dump-covid19_tracker-202404071850.sql
+++ dump-covid19_tracker-202404071850.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `consultaapi`
 --
 
+CREATE DATABASE `covid19_tracker`;
+
+USE `covid19_tracker`;
+
 DROP TABLE IF EXISTS `consultaapi`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
